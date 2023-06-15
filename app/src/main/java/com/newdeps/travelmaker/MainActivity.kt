746bb0c4f2dde0aa769da2000fcf7c129c4d8a15
younminchan <dbsalscks0@naver.com>
2023-06-15 16:58:19 +0900

    네이버 지도 생성 / 마커표시 기능 작업

--- app/src/main/java/com/newdeps/travelmaker/MainActivity.kt
+++ app/src/main/java/com/newdeps/travelmaker/MainActivity.kt
@@ -1,17 +1,80 @@
 package com.newdeps.travelmaker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.MarkerIcons
 import com.newdeps.travelmaker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var naverMap: NaverMap
+    var clickMarker = Marker()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+       naverMapInit()
     }
+
+    private fun naverMapInit() {
+        // Initialize NaverMapSdk with your API key
+        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(getString(R.string.naver_map_client_key))
+
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.naver_map_view) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.naver_map_view, it).commit()
+            }
+
+        // MapFragment가 준비되면 지도 초기화 작업 수행
+        mapFragment.getMapAsync { naverMap ->
+            this.naverMap = naverMap
+            // 지도 초기화 작업 수행
+            // 예: 마커 추가, 이벤트 리스너 등록 등
+            naverMap.apply {
+                mapType = NaverMap.MapType.Basic
+            }
+
+            moveMap(37.483725, 126.876613) //롯데정보통신 본사
+            setMarker(37.483725, 126.876613) //롯데정보통신 본사 마커 표시
+            mapClickListener()
+        }
+    }
+    private fun mapClickListener() {
+        /** 롱클릭 마커 표시 */
+        naverMap.setOnMapLongClickListener { point, coord ->
+            Toast.makeText(this, "${coord.latitude}, ${coord.longitude}", Toast.LENGTH_SHORT).show()
+
+            clickMarker.map = null //기존 마커 삭제
+            setMarker(coord.latitude, coord.longitude)
+        }
+    }
+
+    /** 지도 이동 */
+    private fun moveMap(lat: Double, lng: Double) {
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(lat, lng))
+        naverMap.moveCamera(cameraUpdate)
+    }
+
+    /** 마커 생성 */
+    private fun setMarker(lat: Double, lng: Double) {
+        clickMarker = Marker()
+        clickMarker.position = LatLng(lat, lng) //위치설정
+        clickMarker.icon = MarkerIcons.BLACK //색상
+        clickMarker.iconTintColor = Color.RED //색상 Tint
+        clickMarker.map = naverMap
+    }
+
+
 
 }
